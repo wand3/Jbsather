ab7ac9ec42d5e779b0498ba2f950183d30821302
wand3 <godswillbaiyewu@gmail.com>
2026-03-23 03:26:15 +0100

    TS, Node, Express init

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http') 
-const port = 3000 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World LFG\n')
-  }).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-  })
